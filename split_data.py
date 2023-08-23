da635833cea65003f5ce83c4b59cae1bcb852eb7
Animesh Solanki <92034910+AnimeshSolanki@users.noreply.github.com>
2023-08-23 22:43:52 +0530

    Add files via upload

---
+++ split_data.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+from sklearn.model_selection import train_test_split
+
+source_dir="C:\Users\dell\OneDrive\Desktop\FlowerRecog\flowers"
+train_dir = "C:\Users\dell\OneDrive\Desktop\FlowerRecog\splits"
+val_dir = "C:\Users\dell\OneDrive\Desktop\FlowerRecog\splits" 
+
